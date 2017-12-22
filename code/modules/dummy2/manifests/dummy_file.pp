7bf5dcd04a6c4d7f3770786a77c3941e6754a76b
Kong To <newlight77@gmail.com>
2017-12-22 19:43:24 +0100

    playing with dummy module'

--- code/modules/dummy2/manifests/dummy_file.pp
+++ code/modules/dummy2/manifests/dummy_file.pp
@@ -1,13 +1,15 @@
 define dummy2::dummy_file (
+    # $name, # not allowed to redefine a built-in parameter
     $server_name,
     $nbinstances
 ) {
+    $prefix = "param"
+    $list = "<%= a=[]; (1..@nbinstances).step(1) do |i| a+=['$prefix-%s@%s' % [@title,i]] end; a.join(',') %>"
+    $params_to_install = split(inline_template($list), ',')
 
     file { "/etc/dummy${server_name}.txt":
         ensure => present,
         content => template("dummy2/dummy.txt.erb"),
         mode => "644"
     }
-
-    #$consumers_to_install = split(inline_template("<%= a=[];(1..@nb_instances).step(1) do |i| a+=['posc-events-%s@%s' % [@title,i]] end;a.join(',') %>"), ',')
 }
